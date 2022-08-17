ff4782f9f497fb858096bbad501ab6ccea589a8d
Vlad Barsukov <me@vladbarsukov.ru>
2022-08-17 09:10:54 +0300

    update
    update

--- script/script.js
+++ script/script.js
@@ -1,28 +1,5 @@
 /*
 
-let offset = 0;
-const track = document.querySelector('.about__slider-track');
-const btnPrev = document.querySelector('#btnPrev');
-const btnNext = document.querySelector('#btnNext');
-
-btnNext.addEventListener('click', () => {
-  offset += 1440;
-  if (offset > 7200) {
-    offset = 0
-  }
-  track.style.left = -offset + "px";
-});
-
-btnPrev.addEventListener('click', () => {
-  offset -= 1440;
-  if (offset < 0) {
-    offset = 7200
-  }
-  track.style.left = offset + "px";
-});
-
-*/
-
 const items = document.querySelectorAll('.about__slider-item');
 const btnPrev = document.querySelector('#btnPrev');
 const btnNext = document.querySelector('#btnNext');
@@ -54,28 +31,99 @@ btnPrev.addEventListener('click', () => {
   }
 
 });
-
-
-
-/*
-btnNext.addEventListener('click', () => {
-  items.forEach(function (item) {
-    ++i
-    items[i+1].classList.remove("block");
-    i = 0;
-    items[i].classList.add("block");
-  });
-});
 */
 
 
 
-const subMenu = document.querySelector('.sub-menu_hidden')
-const subMenuBtn = document.querySelector('#menuBtn')
-function openMenu(subMenu) {
-  subMenu.classList.remove("sub-menu_hidden");
-}
-subMenuBtn.addEventListener('click', () => {
-  subMenu.classList.toggle("sub-menu_hidden");
+//Шторка меню
 
+const header = document.querySelector('.header')
+const subMenuHidden = document.querySelector('.sub-menu_hidden')
+const subMenuBtn = document.querySelector('#menuBtn')
+const subMenu = document.querySelector('.sub-menu')
+
+const toggleMenu = function () {
+  subMenuHidden.classList.toggle("sub-menu_hidden");
+}
+
+subMenuBtn.addEventListener("click", function (e) {
+  e.stopPropagation();
+  toggleMenu();
 });
+
+document.addEventListener("click", function (e) {
+  const target = e.target;
+  const its_menu = target === subMenu || subMenu.contains(target);
+  const its_btnMenu = target === header || header.contains(target);
+  const menu_is_active = !subMenu.classList.contains("sub-menu_hidden");
+
+  if (!its_menu && !its_btnMenu && menu_is_active) {
+    toggleMenu();
+  }
+});
+
+
+
+
+
+
+// Слайдер с анимациями
+const items = document.querySelectorAll('.about__slider-item');
+const btnPrev = document.querySelector('#btnPrev');
+const btnNext = document.querySelector('#btnNext');
+// счетчик
+let btnPagination =  document.querySelector('#pagination');
+
+let savedIndex = 0;
+
+function printPagination() {
+  btnPagination.textContent = `${savedIndex + 1}/${items.length}`;
+}
+
+function calcSlideNext() {
+  savedIndex = savedIndex += 1
+  savedIndex = items.length === savedIndex ? 0 : savedIndex;
+  items.forEach(el => el.classList.remove('block'));
+  items.forEach(el => el.classList.remove('block_prev'));
+  items[savedIndex].classList.add('block');
+}
+
+function calcSlidePrev() {
+  savedIndex = savedIndex -= 1
+  savedIndex = savedIndex < 0 ? items.length - 1 : savedIndex;
+  items.forEach(el => el.classList.remove('block_prev'));
+  items.forEach(el => el.classList.remove('block'));
+  items[savedIndex].classList.add('block_prev');
+}
+
+const clickSliderHandlerNext = () => {
+  calcSlideNext();
+  printPagination();
+};
+
+
+const clickSliderHandlerPrev = () => {
+  calcSlidePrev();
+  printPagination();
+};
+
+btnPrev.addEventListener('click', clickSliderHandlerPrev);
+btnNext.addEventListener('click', clickSliderHandlerNext);
+
+
+
+
+/*
+const clickSliderHandler = (operation) => {
+  !function calcSlide() {
+    savedIndex = operation === 'plus' ? savedIndex += 1 : savedIndex -= 1;
+    savedIndex = items.length === savedIndex ? 0 : savedIndex;
+    savedIndex = savedIndex < 0 ? items.length - 1 : savedIndex;
+    items.forEach(el => el.classList.remove('block'));
+    items[savedIndex].classList.add('block');
+  }();
+  !function printPagination() {
+    btnPagination.textContent = `${savedIndex + 1}/${items.length}`;
+  }();
+};
+*/
